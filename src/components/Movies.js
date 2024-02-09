0ffe5ca118ed581fd64c3d519d003de656cbf3bd
Brian <briomwaura@gmail.com>
2024-02-09 14:46:00 +0300

    assignment complete

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,23 @@
-import React from "react";
-import { movies } from "../data";
+import React from 'react';
+import moviesData from '../data';
 
-function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
+const Movies = () => {
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {moviesData.map((movie) => (
+        <div key={movie.id}>
+          <h2>{movie.title}</h2>
+          <p>Time: {movie.time}</p>
+          <ul>
+            {movie.genres.map((genre, index) => (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Movies;
